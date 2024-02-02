8cec8f620c519b5c5938ccf490f0aa779c4e79ed
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-02-02 12:10:32 -0600

    Update dependencies and configuration files

---
+++ app/api/edge/route.ts
@@ -0,0 +1,27 @@
+export const runtime = 'edge'; // 'nodejs' is the default
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+// process.env.PASS
+export async function GET(request: Request) {
+
+    try {
+        const url = 'https://activititracker-rest.onrender.com/api/jobs/desactivateUsers'
+
+        const result = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // The boddy needs a pass to be sent
+            body: JSON.stringify({ pass: process.env.PASS }),
+        });
+
+        if (result.ok) return new Response('Success', { status: 200 });
+        
+        return new Response('Error', { status: 500 });
+        
+    } catch (e) {
+        const error = e as Error;
+        return new Response('Error: ' + error.message, { status: 500 });
+    }
+}
