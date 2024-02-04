af08a04248442a63eba92e6ec2a1222609b27f20
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-02-04 15:05:30 -0600

    Debug console.logs

--- app/api/edge/route.ts
+++ app/api/edge/route.ts
@@ -3,10 +3,12 @@ export const dynamic = 'force-dynamic'; // static by default, unless reading the
 
 // process.env.PASS
 export async function GET(request: Request) {
+    console.log("Request init")
 
     try {
         const url = 'https://activititracker-rest.onrender.com/api/jobs/desactivateUsers'
 
+        console.log("Requesting to backend")
         const result = await fetch(url, {
             method: 'POST',
             headers: {
@@ -16,11 +18,16 @@ export async function GET(request: Request) {
             body: JSON.stringify({ pass: process.env.PASS }),
         });
 
-        if (result.ok) return new Response('Success', { status: 200 });
+        if (result.ok) {
+            console.log("Success")
+            return new Response('Success', { status: 200 });
+        }
         
+        console.log("Error in backend request")
         return new Response('Error', { status: 500 });
         
     } catch (e) {
+        console.error(e);
         return new Response('Error', { status: 500 });
     }
 }
