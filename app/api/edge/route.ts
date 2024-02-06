1f62a18c65857a3f64d0c83189ddd87d0131d596
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-02-06 00:28:17 -0600

    Refactor backend request in route.ts

--- app/api/edge/route.ts
+++ app/api/edge/route.ts
@@ -9,22 +9,19 @@ export async function GET(request: Request) {
         const url = 'https://activititracker-rest.onrender.com/api/jobs/desactivateUsers'
 
         console.log("Requesting to backend")
-        const result = await fetch(url, {
+        const req = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             // The boddy needs a pass to be sent
             body: JSON.stringify({ pass: process.env.PASS }),
-        });
+        })
 
-        if (result.ok) {
-            console.log("Success")
-            return new Response('Success', { status: 200 });
-        }
-        
-        console.log("Error in backend request")
-        return new Response('Error', { status: 500 });
+        const res = await req.json()
+
+        console.log("Request done", res)
+        return new Response(JSON.stringify(res), { status: 200 });
         
     } catch (e) {
         console.error(e);
